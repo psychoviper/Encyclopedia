c2d42d01eaae93e1df0a4959a8cef030cf6e9321
psychoviper <86225058+psychoviper@users.noreply.github.com>
2022-12-09 17:04:37 +0530

    Update settings.py

--- wiki/settings.py
+++ wiki/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','encyclopedia-1.herokuapp.com','.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['127.0.0.1','encyclopedia-1.herokuapp.com',]
 
 
 # Application definition
@@ -122,7 +122,4 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
-DEFAULT_AUTO_FIELD = 'dgango.db.models.BigAuto Field'
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+
